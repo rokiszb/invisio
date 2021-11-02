b8b63741cb1f4c69469010bc24920840e8cd0af9
rokas.lakstauskas <you@example.com>
2021-11-02 13:36:59 +0200

    sol1

--- src/Service/ProductResponseResolver.php
+++ src/Service/ProductResponseResolver.php
@@ -22,26 +22,22 @@ class ProductResponseResolver
 
         if (in_array($parameter1, ProductConfig::PRODUCT_GROUP_1, true)) {
             $productResponse->setParameter1([$parameter1]);
-            $group2 = ProductConfig::PRODUCT_GROUP_2;
             if ($this->product->hasIncompatibleProducts($parameter1)) {
                 $incompatible = $this->product->getIncompatibleProducts($parameter1);
-                $group2 = array_diff($group2, [$incompatible]);
+                $group2 = array_diff(ProductConfig::PRODUCT_GROUP_2, [$incompatible]);
+                $productResponse->setParameter2($group2);
             }
-
-            $productResponse->setParameter2($group2);
 
             return $productResponse;
         }
 
         if (in_array($parameter2, ProductConfig::PRODUCT_GROUP_2, true)) {
             $productResponse->setParameter2([$parameter2]);
-            $group1 = ProductConfig::PRODUCT_GROUP_1;
             if ($this->product->hasIncompatibleProducts($parameter2)) {
                 $incompatible = $this->product->getIncompatibleProducts($parameter2);
-                $group1 = array_diff($group1, [$incompatible]);
+                $group1 = array_diff(ProductConfig::PRODUCT_GROUP_1, [$incompatible]);
+                $productResponse->setParameter1($group1);
             }
-
-            $productResponse->setParameter1($group1);
 
             return $productResponse;
         }
